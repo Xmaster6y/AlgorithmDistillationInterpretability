edc85c34529247794295ada66312b59a51a1dd33
victorLevoso <vlfernandez@esei.uvigo.es>
2023-04-12 06:14:44 +0200

    Added AD folder

---
+++ src/algorithm_distillation_transformer/train.py
@@ -0,0 +1,494 @@
+import os
+from argparse import Namespace
+
+import gymnasium.vector
+import numpy as np
+import pytest
+import torch as t
+import torch.nn as nn
+from einops import rearrange
+from torch.utils.data import DataLoader, random_split
+from torch.utils.data.sampler import WeightedRandomSampler
+from tqdm import tqdm
+
+import wandb
+from src.config import EnvironmentConfig
+from src.models.trajectory_transformer import (
+    CloneTransformer,
+    DecisionTransformer,
+    TrajectoryTransformer,
+)
+
+from .offline_dataset import TrajectoryDataset
+from .utils import get_max_len_from_model_type
+
+
+def train(
+    model: TrajectoryTransformer,
+    trajectory_data_set: TrajectoryDataset,
+    env,
+    make_env,
+    batch_size=128,
+    lr=0.0001,
+    weight_decay=0.0,
+    device="cpu",
+    track=False,
+    train_epochs=100,
+    test_epochs=10,
+    test_frequency=10,
+    eval_frequency=10,
+    eval_episodes=10,
+    initial_rtg=[0.0, 1.0],
+    eval_max_time_steps=100,
+    eval_num_envs=8,
+):
+    loss_fn = nn.CrossEntropyLoss()
+    model = model.to(device)
+    optimizer = t.optim.Adam(
+        model.parameters(), lr=lr, weight_decay=weight_decay
+    )
+
+    train_dataset, test_dataset = random_split(
+        trajectory_data_set, [0.90, 0.10]
+    )
+
+    # Create the train DataLoader
+    train_sampler = WeightedRandomSampler(
+        weights=trajectory_data_set.sampling_probabilities[
+            train_dataset.indices
+        ],
+        num_samples=len(train_dataset),
+        replacement=True,
+    )
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler
+    )
+
+    # Create the test DataLoader
+    test_sampler = WeightedRandomSampler(
+        weights=trajectory_data_set.sampling_probabilities[
+            test_dataset.indices
+        ],
+        num_samples=len(test_dataset),
+        replacement=True,
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=batch_size, sampler=test_sampler
+    )
+
+    train_batches_per_epoch = len(train_dataloader)
+    pbar = tqdm(range(train_epochs))
+    for epoch in pbar:
+        for batch, (s, a, r, d, rtg, ti, m) in enumerate(train_dataloader):
+            total_batches = epoch * train_batches_per_epoch + batch
+
+            model.train()
+
+            if model.transformer_config.time_embedding_type == "linear":
+                ti = ti.to(t.float32)
+
+            a[a == -10] = env.action_space.n  # dummy action for padding
+
+            optimizer.zero_grad()
+
+            if isinstance(model, DecisionTransformer):
+                action = a[:, :-1].unsqueeze(-1) if a.shape[1] > 1 else None
+                _, action_preds, _ = model.forward(
+                    states=s,
+                    # remove last action
+                    actions=action,
+                    rtgs=rtg[:, :-1],  # remove last rtg
+                    timesteps=ti.unsqueeze(-1),
+                )
+            elif isinstance(model, CloneTransformer):
+                _, action_preds = model.forward(
+                    states=s,
+                    # remove last action
+                    actions=a[:, :-1].unsqueeze(-1)
+                    if a.shape[1] > 1
+                    else None,
+                    timesteps=ti.unsqueeze(-1),
+                )
+
+            action_preds = rearrange(action_preds, "b t a -> (b t) a")
+            a_exp = rearrange(a, "b t -> (b t)").to(t.int64)
+
+            # ignore dummy action
+            loss = loss_fn(
+                action_preds[a_exp != env.action_space.n],
+                a_exp[a_exp != env.action_space.n],
+            )
+
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_description(f"Training DT: {loss.item():.4f}")
+
+            if track:
+                wandb.log({"train/loss": loss.item()}, step=total_batches)
+                tokens_seen = (
+                    (total_batches + 1)
+                    * batch_size
+                    * (model.transformer_config.n_ctx // 3)
+                )
+                wandb.log(
+                    {"metrics/tokens_seen": tokens_seen}, step=total_batches
+                )
+
+        # # at test frequency
+        if epoch % test_frequency == 0:
+            test(
+                model=model,
+                dataloader=test_dataloader,
+                env=env,
+                epochs=test_epochs,
+                track=track,
+                batch_number=total_batches,
+            )
+
+        eval_env_config = EnvironmentConfig(
+            env_id=env.spec.id,
+            capture_video=True,
+            max_steps=min(
+                model.environment_config.max_steps, eval_max_time_steps
+            ),
+            fully_observed=False,
+            one_hot_obs=(trajectory_data_set.observation_type == "one_hot"),
+            view_size=env.observation_space["image"].shape[0]
+            if "image" in list(env.observation_space.keys())
+            else 7,
+        )
+
+        eval_env_func = make_env(
+            config=eval_env_config,
+            seed=batch,
+            idx=0,
+            run_name=f"dt_eval_videos_{batch}",
+        )
+
+        if epoch % eval_frequency == 0:
+            for rtg in initial_rtg:
+                evaluate_dt_agent(
+                    env_id=env.spec.id,
+                    model=model,
+                    env_func=eval_env_func,
+                    trajectories=eval_episodes,
+                    track=track,
+                    batch_number=total_batches,
+                    initial_rtg=float(rtg),
+                    device=device,
+                    num_envs=eval_num_envs,
+                )
+
+    return model
+
+
+@pytest.mark.skip(reason="This is not a test")
+def test(
+    model: TrajectoryTransformer,
+    dataloader: DataLoader,
+    env,
+    epochs=10,
+    track=False,
+    batch_number=0,
+):
+    model.eval()
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    loss = 0
+    n_correct = 0
+    n_actions = 0
+
+    pbar = tqdm(range(epochs))
+    test_batches_per_epoch = len(dataloader)
+
+    for epoch in pbar:
+        for batch, (s, a, r, d, rtg, ti, m) in enumerate(dataloader):
+            if model.transformer_config.time_embedding_type == "linear":
+                ti = ti.to(t.float32)
+
+            a[a == -10] = env.action_space.n
+
+            if isinstance(model, DecisionTransformer):
+                _, action_preds, _ = model.forward(
+                    states=s,
+                    actions=a[:, :-1].unsqueeze(-1)
+                    if a.shape[1] > 1
+                    else None,
+                    rtgs=rtg[:, :-1],
+                    timesteps=ti.unsqueeze(-1),
+                )
+            elif isinstance(model, CloneTransformer):
+                _, action_preds = model.forward(
+                    states=s,
+                    # remove last action
+                    actions=a[:, :-1].unsqueeze(-1)
+                    if a.shape[1] > 1
+                    else None,
+                    timesteps=ti.unsqueeze(-1),
+                )
+
+            action_preds = rearrange(action_preds, "b t a -> (b t) a")
+            a_exp = rearrange(a, "b t -> (b t)").to(t.int64)
+
+            a_hat = t.argmax(action_preds, dim=-1)
+            a_exp = rearrange(a, "b t -> (b t)").to(t.int64)
+
+            action_preds = action_preds[a_exp != env.action_space.n]
+            a_hat = a_hat[a_exp != env.action_space.n]
+            a_exp = a_exp[a_exp != env.action_space.n]
+
+            n_actions += a_exp.shape[0]
+            n_correct += (a_hat == a_exp).sum()
+            loss += loss_fn(action_preds, a_exp)
+
+            accuracy = n_correct.item() / n_actions
+            pbar.set_description(f"Testing DT: Accuracy so far {accuracy:.4f}")
+
+    mean_loss = loss.item() / epochs * test_batches_per_epoch
+
+    if track:
+        wandb.log({"test/loss": mean_loss}, step=batch_number)
+        wandb.log({"test/accuracy": accuracy}, step=batch_number)
+
+    return mean_loss, accuracy
+
+
+def evaluate_dt_agent(
+    env_id: str,
+    model: TrajectoryTransformer,
+    env_func,
+    trajectories=300,
+    track=False,
+    batch_number=0,
+    initial_rtg=0.98,
+    use_tqdm=True,
+    device="cpu",
+    num_envs=8,
+):
+    model.eval()
+
+    env = gymnasium.vector.SyncVectorEnv([env_func for _ in range(num_envs)])
+    video_path = os.path.join("videos", env.envs[0].run_name)
+
+    if not hasattr(model, "transformer_config"):
+        model.transformer_config = Namespace(
+            n_ctx=model.n_ctx,
+            time_embedding_type=model.time_embedding_type,
+        )
+
+    max_len = get_max_len_from_model_type(
+        model_type="decision_transformer"
+        if isinstance(model, DecisionTransformer)
+        else "clone_transformer",
+        n_ctx=model.transformer_config.n_ctx,
+    )
+
+    traj_lengths = []
+    rewards = []
+    n_terminated = 0
+    n_truncated = 0
+    reward_total = 0
+    n_positive = 0
+
+    if not os.path.exists(video_path):
+        os.makedirs(video_path)
+
+    videos = [i for i in os.listdir(video_path) if i.endswith(".mp4")]
+    for video in videos:
+        os.remove(os.path.join(video_path, video))
+    videos = [i for i in os.listdir(video_path) if i.endswith(".mp4")]
+
+    if use_tqdm:
+        pbar = tqdm(range(trajectories), desc="Evaluating DT")
+        pbar_it = iter(pbar)
+    else:
+        pbar = range(trajectories)
+
+    # each env will get its own seed by incrementing on the given seed
+    obs, _ = env.reset(seed=0)
+    obs = t.tensor(obs["image"]).unsqueeze(1)
+    rtg = rearrange(t.ones(num_envs, dtype=t.int) * initial_rtg, "e -> e 1 1")
+    a = rearrange(t.zeros(num_envs, dtype=t.int), "e -> e 1 1")
+    timesteps = rearrange(t.zeros(num_envs, dtype=t.int), "e -> e 1 1")
+
+    obs = obs.to(device)
+    rtg = rtg.to(device)
+    a = a.to(device)
+    timesteps = timesteps.to(device)
+
+    if model.transformer_config.time_embedding_type == "linear":
+        timesteps = timesteps.to(t.float32)
+
+    # get first action
+    if isinstance(model, DecisionTransformer):
+        state_preds, action_preds, reward_preds = model.forward(
+            states=obs, actions=None, rtgs=rtg, timesteps=timesteps
+        )
+    elif isinstance(model, CloneTransformer):
+        state_preds, action_preds = model.forward(
+            states=obs, actions=None, timesteps=timesteps
+        )
+    else:  # it's probably a legacy model in which case the interface is:
+        state_preds, action_preds, reward_preds = model.forward(
+            states=obs, actions=a, rtgs=rtg, timesteps=timesteps
+        )
+
+    new_action = t.argmax(action_preds, dim=-1).squeeze(-1)
+    new_obs, new_reward, terminated, truncated, info = env.step(new_action)
+
+    current_trajectory_length = t.ones(num_envs, dtype=t.int)
+    while n_terminated + n_truncated < trajectories:
+        # concat init obs to new obs
+        obs = t.cat(
+            [obs, t.tensor(new_obs["image"]).unsqueeze(1).to(device)], dim=1
+        )
+
+        # add new reward to init reward
+        rtg = t.cat(
+            [
+                rtg,
+                rtg[:, -1:, :]
+                - rearrange(t.tensor(new_reward).to(device), "e -> e 1 1"),
+            ],
+            dim=1,
+        )
+
+        # add new timesteps
+        timesteps = t.cat(
+            [
+                timesteps,
+                rearrange(current_trajectory_length.to(device), "e -> e 1 1"),
+            ],
+            dim=1,
+        )
+
+        if model.transformer_config.time_embedding_type == "linear":
+            timesteps = timesteps.to(t.float32)
+
+        a = t.cat([a, rearrange(new_action, "e -> e 1 1")], dim=1)
+
+        # truncations:
+        obs = obs[:, -max_len:] if obs.shape[1] > max_len else obs
+        actions = (
+            a[:, -(obs.shape[1] - 1) :]
+            if (a.shape[1] > 1 and max_len > 1)
+            else None
+        )
+        timesteps = (
+            timesteps[:, -max_len:]
+            if timesteps.shape[1] > max_len
+            else timesteps
+        )
+        rtg = rtg[:, -max_len:] if rtg.shape[1] > max_len else rtg
+
+        if isinstance(model, DecisionTransformer):
+            state_preds, action_preds, reward_preds = model.forward(
+                states=obs, actions=actions, rtgs=rtg, timesteps=timesteps
+            )
+        elif isinstance(model, CloneTransformer):
+            state_preds, action_preds = model.forward(
+                states=obs, actions=actions, timesteps=timesteps
+            )
+        else:  # it's probably a legacy model in which case the interface is:
+            steps = model.transformer_config.n_ctx // 3
+            state_preds, action_preds, reward_preds = model.forward(
+                states=obs[:, -steps:],
+                actions=a[:, -steps:],
+                rtgs=rtg[:, -steps:],
+                timesteps=timesteps[:, -steps:],
+            )
+
+        new_action = t.argmax(action_preds, dim=-1).squeeze(-1)
+        if new_action.dim() > 1:
+            new_action = new_action[:, -1]
+        # convert to numpy
+        new_obs, new_reward, terminated, truncated, info = env.step(new_action)
+        # print(f"took action  {action} at timestep {i} for reward {new_reward}")
+
+        n_positive = n_positive + sum(new_reward > 0)
+        reward_total += sum(new_reward)
+        n_terminated += sum(terminated)
+        n_truncated += sum(truncated)
+
+        if use_tqdm:
+            pbar.set_description(
+                f"Evaluating DT: Finished running {n_terminated + n_truncated} episodes."
+                f"Current episodes are at timestep {current_trajectory_length.tolist()} for reward {new_reward}"
+            )
+
+        dones = np.logical_or(terminated, truncated)
+        current_trajectory_length += np.invert(dones)
+        traj_lengths.extend(current_trajectory_length[dones].tolist())
+        rewards.extend(new_reward[dones])
+        current_trajectory_length[dones] = 0
+
+        if np.any(dones):
+            if use_tqdm:
+                [next(pbar_it, None) for _ in range(sum(dones))]
+
+            current_videos = [
+                i for i in os.listdir(video_path) if i.endswith(".mp4")
+            ]
+            if track and (
+                len(current_videos) > len(videos)
+            ):  # we have a new video
+                new_videos = [i for i in current_videos if i not in videos]
+                for new_video in new_videos:
+                    path_to_video = os.path.join(video_path, new_video)
+                    wandb.log(
+                        {
+                            f"media/video/{initial_rtg}/": wandb.Video(
+                                path_to_video,
+                                fps=4,
+                                format="mp4",
+                                caption=f"{env_id}, after {n_terminated + n_truncated} episodes, reward {new_reward}, rtg {initial_rtg}",
+                            )
+                        },
+                        step=batch_number,
+                    )
+            videos = current_videos  # update videos
+
+    collected_trajectories = n_terminated + n_truncated
+
+    statistics = {
+        "initial_rtg": initial_rtg,
+        "prop_completed": n_terminated / collected_trajectories,
+        "prop_truncated": n_truncated / collected_trajectories,
+        "mean_reward": reward_total / collected_trajectories,
+        "prop_positive_reward": n_positive / collected_trajectories,
+        "mean_traj_length": sum(traj_lengths) / collected_trajectories,
+        "traj_lengths": traj_lengths,
+        "rewards": rewards,
+    }
+
+    env.close()
+    if track:
+        # log statistics at batch number but prefix with eval
+        for key, value in statistics.items():
+            if key == "initial_rtg":
+                continue
+            if key == "traj_lengths":
+                wandb.log(
+                    {
+                        f"eval/{str(initial_rtg)}/traj_lengths": wandb.Histogram(
+                            value
+                        )
+                    },
+                    step=batch_number,
+                )
+            elif key == "rewards":
+                wandb.log(
+                    {
+                        f"eval/{str(initial_rtg)}/rewards": wandb.Histogram(
+                            value
+                        )
+                    },
+                    step=batch_number,
+                )
+            wandb.log(
+                {f"eval/{str(initial_rtg)}/" + key: value}, step=batch_number
+            )
+
+    return statistics
---
+++ src/algorithm_distillation_transformer/utils.py
@@ -0,0 +1,298 @@
+import argparse
+import json
+import re
+from dataclasses import dataclass, field
+from typing import List
+
+import torch as t
+from minigrid.wrappers import OneHotPartialObsWrapper, RGBImgPartialObsWrapper
+
+from src.config import (
+    EnvironmentConfig,
+    OfflineTrainConfig,
+    TransformerModelConfig,
+)
+from src.models.trajectory_transformer import DecisionTransformer
+
+from .model import DecisionTransformer as DecisionTransformerLegacy
+from .offline_dataset import TrajectoryDataset
+
+
+@dataclass
+class DTArgs:
+    exp_name: str = "Dev"
+    d_model: int = 128
+    trajectory_path: str = "trajectories/MiniGrid-Dynamic-Obstacles-8x8-v0c8c5dccc-b418-492e-bdf8-2c21256cd9f3.pkl"
+    n_heads: int = 4
+    d_mlp: int = 256
+    n_layers: int = 2
+    n_ctx: int = 3
+    layer_norm: bool = False
+    batch_size: int = 64
+    train_epochs: int = 10
+    test_epochs: int = 3
+    learning_rate: float = 0.0001
+    linear_time_embedding: bool = False
+    pct_traj: float = 1
+    weight_decay: float = 0.001
+    seed: int = 1
+    track: bool = True
+    wandb_project_name: str = "DecisionTransformerInterpretability"
+    wandb_entity: str = None
+    test_frequency: int = 100
+    test_batches: int = 10
+    eval_frequency: int = 100
+    eval_episodes: int = 10
+    initial_rtg: List[float] = field(default_factory=lambda: [0.0, 1.0])
+    prob_go_from_end: float = 0.1
+    eval_max_time_steps: int = 1000
+    cuda: bool = True
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="Decision Transformer",
+        description="Train a decision transformer on a trajectory dataset.",
+        epilog="The last enemy that shall be defeated is death.",
+    )
+    parser.add_argument("--exp_name", type=str, default="Dev")
+    parser.add_argument("--d_model", type=int, default=128)
+    parser.add_argument("--trajectory_path", type=str)
+    parser.add_argument("--n_heads", type=int, default=4)
+    parser.add_argument("--d_mlp", type=int, default=256)
+    parser.add_argument("--n_layers", type=int, default=2)
+    parser.add_argument("--n_ctx", type=int, default=3)
+    parser.add_argument("--layer_norm", type=bool, default=False)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--train_epochs", type=int, default=10)
+    parser.add_argument("--test_epochs", type=int, default=3)
+    parser.add_argument("--learning_rate", type=float, default=0.0001)
+    parser.add_argument(
+        "--linear_time_embedding",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument("--pct_traj", type=float, default=1)
+    parser.add_argument("--weight_decay", type=float, default=0.001)
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument(
+        "--track",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--wandb_project_name",
+        type=str,
+        default="DecisionTransformerInterpretability",
+    )
+    parser.add_argument("--wandb_entity", type=str, default=None)
+    parser.add_argument("--test_frequency", type=int, default=100)
+    parser.add_argument("--eval_frequency", type=int, default=100)
+    parser.add_argument("--eval_episodes", type=int, default=10)
+    parser.add_argument("--eval_num_envs", type=int, default=8)
+    parser.add_argument(
+        "--initial_rtg",
+        action="append",
+        help="<Required> Set flag",
+        required=False,
+        default=[0, 1],
+    )
+    parser.add_argument("--prob_go_from_end", type=float, default=0.1)
+    parser.add_argument("--eval_max_time_steps", type=int, default=1000)
+    parser.add_argument("--cuda", action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        "--model_type", type=str, default="decision_transformer"
+    )
+    parser.add_argument(
+        "--convert_to_one_hot",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+    )
+    args = parser.parse_args()
+    return args
+
+
+def load_decision_transformer(model_path, env):
+    if model_stored_in_legacy_format(model_path):
+        state_dict = t.load(model_path)
+        if "state_encoder.weight" in state_dict.keys():
+            return load_legacy_decision_transformer(state_dict, env)
+
+        # get number of layers from the state dict
+        num_layers = (
+            max(
+                [
+                    int(re.findall(r"\d+", k)[0])
+                    for k in state_dict.keys()
+                    if "transformer.blocks" in k
+                ]
+            )
+            + 1
+        )
+        d_model = state_dict["reward_embedding.0.weight"].shape[0]
+        d_mlp = state_dict["transformer.blocks.0.mlp.W_out"].shape[0]
+        n_heads = state_dict["transformer.blocks.0.attn.W_O"].shape[0]
+        max_timestep = state_dict["time_embedding.weight"].shape[0] - 1
+        n_ctx = state_dict["transformer.pos_embed.W_pos"].shape[0]
+        layer_norm = "transformer.blocks.0.ln1.w" in state_dict
+
+        if "state_encoder.weight" in state_dict:
+            # otherwise it would be a sequential and wouldn't have this
+            state_embedding_type = "grid"
+
+        if state_dict["time_embedding.weight"].shape[1] == 1:
+            time_embedding_type = "linear"
+        else:
+            time_embedding_type = "embedding"
+
+        environment_config = EnvironmentConfig(
+            env_id=env.unwrapped.spec.id,
+            one_hot_obs=isinstance(
+                env.observation_space, OneHotPartialObsWrapper
+            ),
+            img_obs=isinstance(env.observation_space, RGBImgPartialObsWrapper),
+            view_size=env.unwrapped.observation_space["image"].shape[0],
+            fully_observed=False,
+            capture_video=False,
+            render_mode="rgb_array",
+        )
+
+        transformer_config = TransformerModelConfig(
+            d_model=d_model,
+            n_heads=n_heads,
+            d_mlp=d_mlp,
+            n_layers=num_layers,
+            n_ctx=n_ctx,
+            layer_norm=layer_norm,
+            time_embedding_type=time_embedding_type,
+            state_embedding_type=state_embedding_type,
+        )
+    else:
+        (
+            state_dict,
+            trajectory_data_set,
+            transformer_config,
+            _,
+        ) = load_model_data(model_path)
+
+        if "state_encoder.weight" in state_dict.keys():
+            return load_legacy_decision_transformer(state_dict, env)
+
+        # now we can create the model
+        # model = DecisionTransformer(
+        #     EnvironmentConfig(env.__spec__),
+        # )
+        environment_config = EnvironmentConfig(
+            env_id=trajectory_data_set.metadata["args"]["env_id"],
+            one_hot_obs=trajectory_data_set.observation_type == "one_hot",
+            view_size=trajectory_data_set.metadata["args"]["view_size"],
+            fully_observed=False,
+            capture_video=False,
+            render_mode="rgb_array",
+        )
+
+    model = DecisionTransformer(
+        environment_config=environment_config,
+        transformer_config=transformer_config,
+    )
+
+    model.load_state_dict(state_dict)
+    return model
+
+
+# To maintain backwards compatibility with the old models.
+
+
+def load_legacy_decision_transformer(state_dict, env):
+    # get number of layers from the state dict
+    num_layers = (
+        max(
+            [
+                int(re.findall(r"\d+", k)[0])
+                for k in state_dict.keys()
+                if "transformer.blocks" in k
+            ]
+        )
+        + 1
+    )
+    d_model = state_dict["reward_embedding.0.weight"].shape[0]
+    d_mlp = state_dict["transformer.blocks.0.mlp.W_out"].shape[0]
+    n_heads = state_dict["transformer.blocks.0.attn.W_O"].shape[0]
+    max_timestep = state_dict["time_embedding.weight"].shape[0] - 1
+    n_ctx = state_dict["transformer.pos_embed.W_pos"].shape[0]
+    layer_norm = "transformer.blocks.0.ln1.w" in state_dict
+
+    if "state_encoder.weight" in state_dict:
+        # otherwise it would be a sequential and wouldn't have this
+        state_embedding_type = "grid"
+
+    if state_dict["time_embedding.weight"].shape[1] == 1:
+        time_embedding_type = "linear"
+    else:
+        time_embedding_type = "learned"
+
+    # now we can create the model
+    model = DecisionTransformerLegacy(
+        env=env,
+        n_layers=num_layers,
+        d_model=d_model,
+        d_mlp=d_mlp,
+        state_embedding_type=state_embedding_type,
+        time_embedding_type=time_embedding_type,
+        n_heads=n_heads,
+        max_timestep=max_timestep,
+        n_ctx=n_ctx,
+        layer_norm=layer_norm,
+    )
+
+    model.load_state_dict(state_dict)
+    return model
+
+
+def model_stored_in_legacy_format(model_path):
+    model_info = t.load(model_path)
+    return "model_state_dict" not in model_info
+
+
+def load_model_data(model_path):
+    model_info = t.load(model_path)
+    state_dict = model_info["model_state_dict"]
+    transformer_config = TransformerModelConfig(
+        **json.loads(model_info["transformer_config"])
+    )
+    transformer_config.device = t.device(transformer_config.device)
+    offline_config = OfflineTrainConfig(
+        **json.loads(model_info["offline_config"])
+    )
+    offline_config.device = t.device(offline_config.device)
+
+    trajectory_data_set = TrajectoryDataset(
+        trajectory_path=offline_config.trajectory_path,
+        max_len=transformer_config.n_ctx // 3,
+        pct_traj=offline_config.pct_traj,
+        prob_go_from_end=offline_config.prob_go_from_end,
+        device=transformer_config.device,
+    )
+
+    return state_dict, trajectory_data_set, transformer_config, offline_config
+
+
+def get_max_len_from_model_type(model_type: str, n_ctx: int):
+    """
+    Ihe max len in timesteps is 3 for decision transformers
+    and 2 for clone transformers since decision transformers
+    have 3 tokens per timestep and clone transformers have 2.
+
+    This is a map between timestep and tokens. We start with one
+    for the most recent state/action and then add another
+    timestep for every 3 tokens for decision transformers and
+    every 2 tokens for clone transformers.
+    """
+    assert model_type in ["decision_transformer", "clone_transformer"]
+    if model_type == "decision_transformer":
+        return 1 + n_ctx // 3
+    else:
+        return 1 + n_ctx // 2
