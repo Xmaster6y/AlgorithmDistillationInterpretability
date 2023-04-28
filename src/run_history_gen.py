b1eed3023a920bf615a32a8db00804ef65beb363
Abhay Sheshadri <abhay.sheshadri@outlook.com>
2023-04-28 17:18:50 -0400

    Added History Generation for DarkRoom/Armed Bandit, Implemented UCB

--- src/run_history_gen.py
+++ src/run_history_gen.py
@@ -1,71 +1,13 @@
-from stable_baselines3.ppo import PPO
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.callbacks import BaseCallback
-from sb3_contrib import RecurrentPPO
-import numpy as np
-from generation import *
 import argparse
 import os
-
-
-class RecordHistory(BaseCallback):
-
-    def __init__(self, file_name):
-        super().__init__()
-        self.rollouts = {
-            "observations": [],
-            "actions"     : [],
-            "rewards"     : [],
-            "dones"       : []
-        }
-        self.file_name = file_name
-
-    def _on_step(self):
-        # Retreive relevant information from locals
-        obs = self.locals["obs_tensor"].cpu().numpy()[0]
-        act = self.locals["actions"]
-        rew = self.locals["rewards"]
-        dones = self.locals["dones"]  # dones = done or truncated
-
-        # Add to dictionary
-        self.rollouts["observations"].append(obs)
-        self.rollouts["actions"].append(act)
-        self.rollouts["rewards"].append(rew)
-        self.rollouts["dones"].append(dones)
-
-    def _on_training_end(self):
-        # Concatenate data to make everything a np array
-        self.rollouts["observations"] = np.array(self.rollouts["observations"])
-        self.rollouts["actions"] = np.array(self.rollouts["actions"])
-        self.rollouts["rewards"] = np.array(self.rollouts["rewards"])
-        self.rollouts["dones"] = np.array(self.rollouts["dones"])
-        # Write to a file
-        np.savez(self.file_name, **self.rollouts)
-
-
-def create_vector_environment(seed, n_states, n_actions, n_envs, env_len):
-    def create_env():
-        # Create DarkKeyDoor environment with fixed seed
-        env = DarkKeyDoor(n_states, n_actions, env_len, seed=seed)
-        env = Monitor(env)
-        return env
-    # Add in a couple other things to assist in training
-    venv = DummyVecEnv([create_env for _ in range(n_envs)])
-    return venv
-
-
-def train_policy(venv, file_name, n_steps, buffer_size):
-    # Trains a recurrent policy to play env with seed
-    # Writes the data to file_name
-    model = RecurrentPPO("MlpLstmPolicy", venv, ent_coef=0.01, verbose=1, n_steps=buffer_size, batch_size=buffer_size)
-    record_history = RecordHistory(file_name)
-    model = model.learn(total_timesteps=n_steps, callback=record_history)
+from utils import *
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--env_id", type=str, required=True, choices=["DarkRoom", "DarkKeyDoor", "ArmedBandit"])
     parser.add_argument("--n_seeds", type=int, default=500)
     parser.add_argument("--seed_start", type=int, default=0)
     parser.add_argument("--n_steps", type=int, default=12_000)
@@ -77,6 +19,26 @@ if __name__ == "__main__":
 
     for env_seed in range(args.seed_start, args.seed_start+args.n_seeds):
         print(f"Training policy {env_seed - args.seed_start}")
-        venv = create_vector_environment(env_seed, args.n_states, args.n_actions, 1, args.max_env_len)
+        venv = create_vector_environment(
+            env_id=args.env_id,
+            n_envs=1, 
+            n_states=args.n_states,
+            n_actions=args.n_actions, 
+            max_steps=args.max_env_len,
+            seed=env_seed
+        )
         file_path = os.path.join(args.path, f"{env_seed}")
-        train_policy(venv, file_path, args.n_steps, args.max_env_len * args.n_rollouts)
+        # We want to use PPO for DarkRoom and DarkKeyRoom, and UCB for MultiArmedBandits
+        if args.env_id in ["DarkRoom", "DarkKeyRoom"]:
+            train_policy(
+                venv=venv,
+                file_name=file_path,
+                n_steps=args.n_steps,
+                buffer_size=args.max_env_len * args.n_rollouts
+            )
+        elif args.env_id in ["ArmedBandit"]:
+            train_ucb(
+                venv=venv,
+                file_name=file_path,
+                n_steps=args.n_steps,
+            )
--- src/utils.py
+++ src/utils.py
@@ -7,13 +7,20 @@ import pickle
 import time
 from typing import Dict
 
-import numpy as np
-import torch
-import torch as t
+from sb3_contrib import RecurrentPPO
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.ppo import PPO
 from typeguard import typechecked
-
+import argparse
+import numpy as np
+import os
+import torch
 import wandb
-from src.config import ConfigJsonEncoder
+
+from generation import *
+from config import ConfigJsonEncoder
 
 
 class TrajectoryWriter:
@@ -142,23 +149,23 @@ def pad_tensor(
                 length - tensor.shape[1],
                 *tensor.shape[2:],
             )
-            pad = t.ones(pad_shape) * pad_token
+            pad = torch.ones(pad_shape) * pad_token
 
             if pad_left:
-                tensor = t.cat([pad, tensor], dim=1)
+                tensor = torch.cat([pad, tensor], dim=1)
             else:
-                tensor = t.cat([tensor, pad], dim=1)
+                tensor = torch.cat([tensor, pad], dim=1)
 
         return tensor
     else:
         if tensor.shape[0] < length:
             pad_shape = (length - tensor.shape[0], *tensor.shape[1:])
-            pad = t.ones(pad_shape) * pad_token
+            pad = torch.ones(pad_shape) * pad_token
 
             if pad_left:
-                tensor = t.cat([pad, tensor], dim=0)
+                tensor = torch.cat([pad, tensor], dim=0)
             else:
-                tensor = t.cat([tensor, pad], dim=0)
+                tensor = torch.cat([tensor, pad], dim=0)
 
         return tensor
 
@@ -202,3 +209,122 @@ class DictList(dict):
     def __setitem__(self, index, d):
         for key, value in d.items():
             dict.__getitem__(self, key)[index] = value
+
+
+class RecordHistory(BaseCallback):
+
+    def __init__(self, file_name):
+        super().__init__()
+        self.rollouts = {
+            "observations": [],
+            "actions"     : [],
+            "rewards"     : [],
+            "dones"       : []
+        }
+        self.file_name = file_name
+
+    def _on_step(self):
+        # Retreive relevant information from locals
+        obs = self.locals["obs_tensor"].cpu().numpy()[0]
+        act = self.locals["actions"]
+        rew = self.locals["rewards"]
+        dones = self.locals["dones"]  # dones = done or truncated
+
+        # Add to dictionary
+        self.rollouts["observations"].append(obs)
+        self.rollouts["actions"].append(act)
+        self.rollouts["rewards"].append(rew)
+        self.rollouts["dones"].append(dones)
+
+    def _on_training_end(self):
+        # Concatenate data to make everything a np array
+        self.rollouts["observations"] = np.array(self.rollouts["observations"])
+        self.rollouts["actions"] = np.array(self.rollouts["actions"])
+        self.rollouts["rewards"] = np.array(self.rollouts["rewards"])
+        self.rollouts["dones"] = np.array(self.rollouts["dones"])
+        # Write to a file
+        np.savez(self.file_name, **self.rollouts)
+
+
+def create_environment_from_id(env_id, n_states, n_actions, max_steps, seed):
+    assert env_id in ["DarkKeyDoor", "DarkRoom", "ArmedBandit"]  # Only supports certain environments
+
+    if env_id == "DarkKeyDoor":
+        env = DarkKeyDoor(n_states, n_actions, max_steps, seed=seed)
+    elif env_id == "DarkRoom":
+        env = DarkRoom(n_states, n_actions, max_steps, seed=seed)
+    elif env_id == "ArmedBandit":
+        assert n_states == 1 and max_steps == 1, "--n_states and --max_env_len should be set to 1"  # Armed Bandit only supports single state
+        env = MultiArmedBandit(n_actions, max_steps, seed=seed)
+
+    return env
+
+
+def create_vector_environment(env_id, n_envs, n_states, n_actions,  max_steps, seed):
+    def create_env():
+        # Create DarkKeyDoor environment with fixed seed
+        env = create_environment_from_id(env_id, n_states, n_actions, max_steps, seed)
+        env = Monitor(env)
+        return env
+    # Add in a couple other things to assist in training
+    venv = DummyVecEnv([create_env for _ in range(n_envs)])
+    return venv
+
+
+def train_policy(venv, file_name, n_steps, buffer_size):
+    # Trains a recurrent policy to play env with seed
+    # Writes the data to file_name
+    model = RecurrentPPO("MlpLstmPolicy", venv, ent_coef=0.01, verbose=1, n_steps=buffer_size, batch_size=buffer_size)
+    record_history = RecordHistory(file_name)
+    model = model.learn(total_timesteps=n_steps, callback=record_history)
+
+
+def train_ucb(venv, file_name, n_steps):
+    # Train model using upper confidence bound algorithm
+    env = venv.get_attr("env")[0]
+    assert env.action_space.n < n_steps, "Not enough steps to run algorithm"
+    # Initialize buffers
+    means = np.zeros((env.action_space.n,))
+    visits = np.zeros((env.action_space.n,))
+    scores = np.zeros((env.action_space.n,))
+    rollouts = {
+        "observations": [],
+        "actions"     : [],
+        "rewards"     : [],
+        "dones"       : []
+    }
+    # Pre-initialize tables
+    for t in range(env.action_space.n):
+        action = t
+        obs, _ = env.reset()
+        obs, reward, done, _, _ = env.step(action)
+        # Add relevant to rollouts
+        rollouts["observations"].append(obs)
+        rollouts["actions"].append(action)
+        rollouts["rewards"].append(reward)
+        rollouts["dones"].append(done)
+        # Update UCB scores
+        means[action] = (means[action] * visits[action] + reward) / (visits[action] + 1)
+        visits[action] += 1
+    scores = means + np.sqrt(np.log(visits.sum()) / visits)
+    # Run UCB
+    for t in range(env.action_space.n, n_steps):
+        action = np.argmax(scores)
+        obs, _ = env.reset()
+        obs, reward, done, _, _ = env.step(action)
+        # Add relevant to rollouts
+        rollouts["observations"].append(obs)
+        rollouts["actions"].append(action)
+        rollouts["rewards"].append(reward)
+        rollouts["dones"].append(done)
+        # Update UCB scores
+        means[action] = (means[action] * visits[action] + reward) / (visits[action] + 1)
+        visits[action] += 1
+        scores = means + np.sqrt(np.log(t + 1) / visits)
+    # Concatenate data to make everything a np array
+    rollouts["observations"] = np.array(rollouts["observations"])
+    rollouts["actions"] = np.array(rollouts["actions"])[:, None]
+    rollouts["rewards"] = np.array(rollouts["rewards"])[:, None]
+    rollouts["dones"] = np.array(rollouts["dones"])[:, None]
+    # Write to a file
+    np.savez(file_name, **rollouts)
