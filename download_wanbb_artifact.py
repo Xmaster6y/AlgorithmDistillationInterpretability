3737a139170e40b2e1e144f4dbd176b7a9798e7a
Edoardo Pona <edoardo.pona@gmail.com>
2023-05-26 18:00:10 +0100

    download any model you want! :smiley:

--- download_wanbb_artifact.py
+++ download_wanbb_artifact.py
@@ -1,4 +1,10 @@
 import wandb
+import sys 
+
+
 api = wandb.Api()
-artifact = api.artifact('algorithm-distillation-interp/Algorithm_distillation_intepretability/testdebug__1__1683169313:v0')
-artifact.download()
+# if user provides a model name, use that, otherwise use the default
+model_name = sys.argv[1] if len(sys.argv) > 1 else 'dark_room_test__1000__1684713463:v25'
+
+artifact = api.artifact(f'algorithm-distillation-interp/Algorithm_distillation_intepretability/{model_name}')
+artifact.download(root='models/')
